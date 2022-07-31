e94788569ba38268fc045a04b40dbcece3b27d06
Venicius Feitosa <feitosa.veniciusf@gmail.com>
2022-07-31 13:16:53 -0300

    colocando parcel

---
+++ src/assets/js/modules.js
@@ -0,0 +1,160 @@
+
+
+export const acais = [
+{
+    volume:"300ml",
+    precoString:"R$ 10,00",
+    preco: 10.00
+},
+{
+    volume:"400ml",
+    precoString:"R$ 12,00",
+    preco: 12.00
+},
+{
+    volume:"600ml",
+    precoString:"R$ 15,00",
+    preco: 15.00
+},
+{
+    volume:"1kg",
+    precoString:"R$ 20,00",
+    preco: 20.00
+}]
+
+export const saborCremes = [
+{
+    sabor:"Sem creme",
+    id:"semCreme"
+}
+,{
+    sabor:"Creme de Ninho",
+    id:"ninho"
+},
+{
+    sabor:"Creme de Oreo",
+    id:"oreo"
+},
+{
+    sabor:"Creme de Morango",
+    id:"morango"
+},
+{
+    sabor:"Creme de Ovomaltine",
+    id:"ovomaltine"
+},
+{
+    sabor:"Creme de Nutella",
+    id:"nutella"
+}]
+
+export const saborAdicionais = [
+{
+    sabor:"Leite em pó",
+    id:"lPo"
+},
+{
+    sabor:"Nutella",
+    id:"nut"
+},
+{
+    sabor:"Sucrilhos",
+    id:"sucri"
+},
+{
+    sabor:"Gotas de chocolate",
+    id:"gChoco"
+},
+{
+    sabor:"Paçoca",
+    id:"pacoca"
+}]
+
+export const saborAdicionaisPagos = [
+{
+    sabor:"Sem adicional pago",
+    id:"semAdiPag",
+    precoString:"R$ 0,00",
+    preco: 0
+}
+,{
+    sabor:"Leite em pó",
+    id:"lPoPAG",
+    precoString:"R$ 0,50",
+    preco:  0.50
+},
+{
+    sabor:"Nutella",
+    id:"nutPAG",
+    precoString:"R$ 1,00",
+    preco:  1.00
+},
+{
+    sabor:"Sucrilhos",
+    id:"sucriPAG",
+    precoString:"R$ 0,50",
+    preco:  0.50
+},
+{
+    sabor:"Gotas de chocolate",
+    id:"gChocoPAG",
+    precoString:"R$ 0,50",
+    preco:  0.50
+},
+{
+    sabor:"Paçoca",
+    id:"pacocaPAG",
+    precoString:"R$ 0,50",
+    preco:  0.50
+}]
+
+export const bairros2 = [
+{bairro:"7 DE SETEMBRO", freteString: null, frete: null},
+{bairro:"ALTIPLANO", freteString: null, frete: null},
+{bairro:"ALTO DO JUCA", freteString: null, frete: null},
+{bairro: "ALVORADA", freteString: null, frete: null},
+{bairro:"AREIAS I", freteString: null, frete: null},
+{bairro:"AREIAS II", freteString: null, frete: null},
+{bairro:"BASTIANA", freteString: null, frete: null},
+{bairro:"BRASILIA", freteString: null, frete: null},
+{bairro:"BUGI", freteString: null, frete: null},
+{bairro:"CAJUEIRO", freteString: null, frete: null},
+{bairro:"CENTRO", freteString: null, frete: null},
+{bairro:"COCOBÓ", freteString: null, frete: null},
+{bairro:"COHAB 1", freteString: null, frete: null},
+{bairro:"COHAB 2", freteString: null, frete: null},
+{bairro:"ESPLANADA I", freteString: null, frete: null},
+{bairro:"ESPLANADA II", freteString: null, frete: null},
+{bairro:"ESPLENDOR", freteString: null, frete: null},
+{bairro:"FILADELFIA", freteString: null, frete: null},
+{bairro:"FLORES", freteString: null, frete: null},
+{bairro:"Fomento", freteString: null, frete: null},
+{bairro:"INDUSTRIAL", freteString: null, frete: null},
+{bairro:"JARDIM IGUATU", freteString: null, frete: null},
+{bairro:"JARDIM OASIS", freteString: "R$ 2,00", frete: 2},
+{bairro:"JOAO PAULO II", freteString: null, frete: null},
+{bairro:"LAGOA PARK", freteString: null, frete: null},
+{bairro:"NOVA CAJAZEIRAS", freteString: null, frete: null},
+{bairro:"Novo Altiplano", freteString: null, frete: null},
+{bairro:"NOVO IGUATU", freteString: null, frete: null},
+{bairro:"PARANA", freteString: null, frete: null},
+{bairro:"PLANALTO", freteString: null, frete: null},
+{bairro:"PRADO", freteString: null, frete: null},
+{bairro:"PREMIER", freteString: null, frete: null},
+{bairro:"SACRAMENTO LOT", freteString: null, frete: null},
+{bairro:"SANTO ANTONIO", freteString: null, frete: null},
+{bairro:"SAO SEBASTIAO", freteString: null, frete: null},
+{bairro:"TABULEIRO", freteString: null, frete: null},
+{bairro:"TERRA BELLA", freteString: null, frete: null},
+{bairro:"VARJOTA", freteString: null, frete: null},
+{bairro:"VENEZA", freteString: null, frete: null},
+{bairro:"VERDE PARK", freteString: null, frete: null},
+{bairro:"VILA CENTENARIO", freteString: null, frete: null},
+{bairro:"VILA CIDAO", freteString: null, frete: null},
+{bairro:"Vila Coqueiro", freteString: null, frete: null},
+{bairro:"VILA DAAE", freteString: null, frete: null},
+{bairro:"VILA MOURA", freteString: null, frete: null},
+{bairro:"VILA NEUMA", freteString: null, frete: null}
+]
+
+export const texto = "%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D%0AMENSSAGEM%20PADR%C3%83O%0A----------------------------------------%0AVolume%3A%20variavel%0A%0ACremes%3A%20variavel%0A%0AAdicionais%3A%20variavel%0A%0AAdicionais%20pagos%3A%20variavel%0A----------------------------------------%0AEndere%C3%A7o%3A%20variavel%20%0A----------------------------------------%0APre%C3%A7o%20dos%20adicionais%20pagos%3A%20variavel%0A%0AFrete%3A%20variavel%20%0A%0APre%C3%A7o%20Total%3A%20variavel%20%0A----------------------------------------%0APagamento%3A%20variavel%20%0A----------------------------------------%0A%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D-%3D"
